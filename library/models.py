db8525b6a22a1f46292517232d4480a2e896b091
Muna Faith <munafaith294@gmail.com>
2025-10-19 15:14:55 +0300

    Initial project setup with Django models and admin

---
+++ library/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Book Model
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    isbn = models.CharField(max_length=13, unique=True)
+    published_date = models.DateField()
+    available_copies = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return self.title
+
+# Loan Model (to track transactions)
+class Loan(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    checkout_date = models.DateTimeField(auto_now_add=True)
+    return_date = models.DateTimeField(null=True, blank=True)
+    is_returned = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.user.username} - {self.book.title}'
