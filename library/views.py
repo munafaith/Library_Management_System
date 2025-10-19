eb3d8a80a83ed8c13e26ae521b059dd6081c9d6d
Muna Faith <munafaith294@gmail.com>
2025-10-19 15:46:35 +0300

    Implement CRUD API endpoints for Books

--- library/views.py
+++ library/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
 
-# Create your views here.
+# This view handles listing all books and creating a new book
+class BookListCreateView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+# This view handles retrieving, updating, and deleting a single book
+class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
